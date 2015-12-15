554ed90f575cd07ef5a37c1bfa82b5696622f381
Emile <emil44d8@edu.eal.dk>
2015-12-15 18:36:23 +0100

    Restarted development on this bot with a higher focus on quality

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Telebot-Ebooks")]
+[assembly: AssemblyTitle("TelegramEbooks-Bot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Telebot-Ebooks")]
+[assembly: AssemblyProduct("TelegramEbooks-Bot")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aea2f649-2ed6-4a7d-b20c-a57992232892")]
+[assembly: Guid("ce3bb8aa-8784-40d2-b59f-bbd27c0eba88")]
 
 // Version information for an assembly consists of the following four values:
 //
